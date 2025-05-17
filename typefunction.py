2223ef3d36a1b92fc94455835650cf79762f47f3
sarvesh <s.sarvesh0712@gmail.com>
2025-05-17 11:33:24 +0530

    Create typefunction.py

---
+++ typefunction.py
@@ -0,0 +1,12 @@
+#variable types in python
+integer = 10
+float = 10.00
+is_true = True
+msg="Hello world!"
+lst=[1,2,3]
+#printing the datatypes of following variables <class,datatype>
+print(type(integer))
+print(type(float))
+print(type(is_true))
+print(type(msg))
+print(type(lst))
