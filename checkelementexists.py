34296c860afc9eae741a78f1ca6445f446baf62c
sarvesh <s.sarvesh0712@gmail.com>
2025-05-19 19:57:22 +0530

    Create checkelementexists.py

---
+++ checkelementexists.py
@@ -0,0 +1,8 @@
+#To return the maximum number of a list
+arr= list(map(int,input().split()))
+#this prints the entered numbers
+print(arr)
+#asking the user to enter a number to check
+x=input("number to check in list")
+#returns a boolean value based on numbers existance
+print(x in arr)
