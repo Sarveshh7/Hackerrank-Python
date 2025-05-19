bc51b5ac30d931abe5ebe3febd9e64f3baf99d24
sarvesh <s.sarvesh0712@gmail.com>
2025-05-19 19:48:08 +0530

    Create maximumnumber.py

---
+++ maximumnumber.py
@@ -0,0 +1,6 @@
+#To return the maximum number of a list
+arr= list(map(int,input().split()))
+#this prints the entered numbers
+print(arr)
+#this prints the maximum of those numbers
+print(max(arr))
